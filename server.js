3705f70dd19a08812b4dc2086dcb9d21aed387f6
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-07 12:15:05 -0500

    Committing initial file structure

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./db');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running @ http://localhost:${PORT}/`);
+    });
+  });
