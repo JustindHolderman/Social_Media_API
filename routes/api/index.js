5c32e01fe283560c1972cb62cb68a70bf00efafb
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-07 13:16:03 -0500

    Moved index from api to routes root. Added thought and user route files. Set up API index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes')
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
