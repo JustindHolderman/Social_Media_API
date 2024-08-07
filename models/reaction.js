98ab3bd75838936c89cd88b85d21fdb2733d485e
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-07 13:08:53 -0500

    Committing reaction model.

--- models/reaction.js
+++ models/reaction.js
@@ -1,1 +1,35 @@
 const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Types.ObjectId(),
+            immutable: true,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                return date ? date.toLocaleString() : '';
+            }
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+module.exports = reactionSchema;
